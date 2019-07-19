f5bb85503796aeb5aed851cadab1369cc51fce1b
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2019-07-19 18:39:23 +0400

    Made some chnages in backend side

--- routes/students.js
+++ routes/students.js
@@ -9,6 +9,11 @@ router.get('/students/:email', passport.authenticate('basic', { session: false }
   function(req, res, next) {
     // TODO: add api handler to check if email/password exists
     // TODO: Response back with user data
+
+    db.getClient().collection("students").findOne({email: req.params.email}, (err, results) => {
+      res.send(results)
+    }) 
+    
 });
 
 /* Create a student account */
